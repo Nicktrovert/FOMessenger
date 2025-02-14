aa7446c326a70271662613406990d8655b8dbae7
Nick Helmers <yolorolo2007@gmail.com>
2025-02-14 15:13:25 +0100

    Query Parser + Login Page (still need to do the code)

---
+++ FOMessenger/Code/Uri/QueryParser.cs
@@ -0,0 +1,19 @@
+﻿namespace FOMessenger.Code.Uri
+{
+    public static class QueryParser
+    {
+        public static Dictionary<string, string> ParseQuery(string query)
+        {
+            Dictionary<string, string> KeyValueQuery = new Dictionary<string, string>();
+            string[] splitQuery = query.Split('?', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < splitQuery.Length; i++)
+            {
+                string[] keyValue = splitQuery[i].Split("=", StringSplitOptions.TrimEntries);
+                string key = keyValue[0].ToLower();
+                string value = keyValue[1];
+                KeyValueQuery.Add(key, value);
+            }
+            return KeyValueQuery;
+        }
+    }
+}
